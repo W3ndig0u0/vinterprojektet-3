49578d8953922ad3cecf2fccb6d3ba90945ab149
W3ndig0 <70271139+W3ndig0u0@users.noreply.github.com>
2022-02-04 20:05:33 +0100

    aaa

--- VinterProjektet/Game.cs
+++ VinterProjektet/Game.cs
@@ -7,42 +7,11 @@ namespace VinterProjektet
   {
     public void Start()
     {
-      // Console.WriteLine("w");
-      //?Vill inte se vad spelaren skriver, bara om en knapp trycks
-      //? så att programmet kan se om det är en av menyn kontroll knapparna.
-
-      //? ConsoelkeyInfo returneras när man gör en Readkey, detta låter oss se vad för knapp man track på
-      // ConsoleKeyInfo keyPressed = Console.ReadKey();
-
-      // if (keyPressed.Key == ConsoleKey.Enter)
-      // {
-      //   Console.WriteLine("ENTER");
-      // }
-      // else if (keyPressed.Key == ConsoleKey.UpArrow)
-      // {
-      //   Console.WriteLine("UP Arrow");
-      // }
-      // else if (keyPressed.Key == ConsoleKey.DownArrow)
-      // {
-      //   Console.WriteLine("DownArrow");
-      // }
-      // else if (keyPressed.Key == ConsoleKey.LeftArrow)
-      // {
-      //   Console.WriteLine("LeftArrow");
-      // }
-      // else if (keyPressed.Key == ConsoleKey.RightArrow)
-      // {
-      //   Console.WriteLine("RightArrow");
-      // }
-      // else
-      // {
-      //   Console.WriteLine("Error");
-      // }
       string promt = "Welcome to the Inventory. What to you want to do?";
       List<string> menuOptions = new List<string>() { "Start", "About", "Quit" };
 
       Menu startMenu = new Menu(promt, menuOptions);
-      startMenu.DisplayOptions();
+      startMenu.Run();
 
       Console.ReadKey(true);
     }
--- VinterProjektet/Menu.cs
+++ VinterProjektet/Menu.cs
@@ -18,22 +18,76 @@ namespace VinterProjektet
       SelectedIndex = 0;
     }
 
+
     // ?Detta rendrar menyn
-    public void DisplayOptions()
+    void DisplayOptions()
     {
       Console.WriteLine(Promt);
       // !Skriver ut allt i listan
       for (int i = 0; i < MenuOptions.Count; i++)
       {
 
+        string currentOptions = MenuOptions[i];
+        string prefix;
 
+        //? SelectedIndex är den menuOptions som använderen valde.
         if (i == SelectedIndex)
         {
-
+          prefix = "*";
+          Console.ForegroundColor = ConsoleColor.Black;
+          Console.BackgroundColor = ConsoleColor.White;
         }
-        string currentOptions = MenuOptions[i];
-        Console.WriteLine("<< " + currentOptions + " >>");
+        else
+        {
+          prefix = " ";
+          Console.ForegroundColor = ConsoleColor.White;
+          Console.BackgroundColor = ConsoleColor.Black;
+        }
+        Console.WriteLine(prefix + "<< " + currentOptions + " >>");
       }
+      Console.ResetColor();
+    }
+
+
+    // ?Koden som körs, låter spelaren ändra selectedIndex
+    public int Run()
+    {
+      ConsoleKey keyPressed;
+      do
+      {
+
+        Console.Clear();
+        DisplayOptions();
+
+        //? ConsoelkeyInfo returneras när man gör en Readkey, detta låter oss se vad för knapp man track på
+        ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+        keyPressed = keyInfo.Key;
+
+        //? Uppdaterar SelectedIndex baserat på vad användaren trycker.
+
+        //?Vill inte se vad spelaren skriver, bara om en knapp trycks
+        //? så att programmet kan se om det är en av menyn kontroll knapparna.
+
+        if (keyPressed == ConsoleKey.UpArrow)
+        {
+          SelectedIndex--;
+          if (SelectedIndex == -1)
+          {
+            SelectedIndex = MenuOptions.Count - 1;
+          }
+        }
+        else if (keyPressed == ConsoleKey.DownArrow)
+        {
+          SelectedIndex++;
+          if (SelectedIndex == MenuOptions.Count)
+          {
+            SelectedIndex = 0;
+          }
+        }
+
+      } while (keyPressed != ConsoleKey.Enter);
+
+      return SelectedIndex;
     }
 
   }
